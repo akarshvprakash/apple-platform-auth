dc36cfd7fedc49d812c3027bc48310ddaf3e60a5
akarshvprakash <akarshvprakash@gmail.com>
2024-06-04 18:06:10 +0530

    sdfsdf

--- pages/api/users/register-apple-ath.ts
+++ pages/api/users/register-apple-ath.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       if (verificationResult.verified && verificationResult.registrationInfo) {
         const { credentialID, credentialPublicKey } = verificationResult.registrationInfo;
         const client = await pool.connect();
-        const query = 'INSERT INTO biometric_credentials (user_id, credentialID, credentialPublicKey) VALUES ($1, $2, $3) RETURNING *';
+        const query = 'INSERT INTO publickkey (user_id, credential_id, public_key) VALUES ($1, $2, $3) RETURNING *';
         const values = [user_id, credentialID, credentialPublicKey];
         await client.query(
             query,
